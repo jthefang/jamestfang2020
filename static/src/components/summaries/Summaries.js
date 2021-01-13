38e99e5c2f304b84478952d0a4fd588a12df7a2f
James Fang <jtotheamesfang@gmail.com>
2021-01-12 21:16:27 -0800

    exhalation + letting go up to chapter 12

--- static/src/components/summaries/Summaries.js
+++ static/src/components/summaries/Summaries.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Collapse, CardBody, Card, CardHeader } from 'reactstrap';
 
-import fifth_mountain from './data/fifth_mountain.json';
-import subtle_art_of_not_giving_a_fuck from './data/subtle_art_of_not_giving_a_fuck.json';
-import when_science_of_perfect_timing from './data/when_science_of_perfect_timing.json';
-import colorless_tsukuru from './data/colorless_tsukuru.json';
+import exhalation from './data/exhalation.json';
 import four_agreements from './data/four_agreements.json';
+import colorless_tsukuru from './data/colorless_tsukuru.json';
+import when_science_of_perfect_timing from './data/when_science_of_perfect_timing.json';
+import subtle_art_of_not_giving_a_fuck from './data/subtle_art_of_not_giving_a_fuck.json';
+import fifth_mountain from './data/fifth_mountain.json';
 
 let books = [
-  four_agreements, colorless_tsukuru, when_science_of_perfect_timing, 
-  subtle_art_of_not_giving_a_fuck, fifth_mountain
+  exhalation, four_agreements, colorless_tsukuru, 
+  when_science_of_perfect_timing, subtle_art_of_not_giving_a_fuck, fifth_mountain
 ];
 
 export default class Summaries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currIdx: -1,
+      cards: [1, 2, 3, 4, 5]
+    };
+  }
+
+  toggle = (e) => {
+    let cardIdx = e.target.dataset.event;
+    this.setState({
+      currIdx: this.state.currIdx === Number(cardIdx) ? -1 : Number(cardIdx)
+    });
+  }
+
   bullets_to_ul(json_bullet_obj, idx_str=0) {
     /** 
      * Returns an unordered list of bullets (<ul> / <li> elements) from a json list object.
@@ -47,18 +63,26 @@ export default class Summaries extends Component {
   }
 
   render() {
-    books = books.map(book => this.json_to_book_object(book))
+    const { currIdx } = this.state;
+    let book_objects = books.map(book => this.json_to_book_object(book));
     return (
-      <Row className="justify-content-center mb-5 pb-5">
-        {books.map((book, index) => (
-          <Col key={index} className="my-5 px-5" lg="7">
-            <h2>{book.title}</h2>
-            <p><small><a href={book.href} target="_blank" rel="noopener noreferrer">Goodreads</a></small></p>
-            <p><em>{book.rating.value}/5: {book.rating.reason}</em></p>
-            {book.bullets}
-          </Col>
-        ))}
+      <Row className="my-5 pb-5 px-1 justify-content-center">
+        <Col xs="12" md="8">
+          {book_objects.map((book, index) => (
+            <Card key={index} className="mb-3">
+              <CardHeader className="btn" onClick={this.toggle} data-event={index}>{book.title}</CardHeader>
+              <Collapse isOpen={currIdx === index}>
+                <CardBody>
+                <h2>{book.title}</h2>
+                <p><small><a href={book.href} target="_blank" rel="noopener noreferrer">Goodreads</a></small></p>
+                <p><em>{book.rating.value}/5: {book.rating.reason}</em></p>
+                {book.bullets}
+                </CardBody>
+              </Collapse>
+            </Card>
+          ))}
+        </Col>
       </Row>
-    )
+    );
   }
 }
